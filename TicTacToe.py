6fc94a5837a380c60f99dac02d957f0ff5a02096
s1mpleTEK <57884979+s1mpleTEK@users.noreply.github.com>
2020-08-02 22:30:50 +0200

    Added ideas of the code conception of this game

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,11 +2,24 @@
 
 import numpy as np
 
+player = 1  #default value
+            #1 means O & 2 means X & 0 means someone win
+
 def set_table():
     table = np.zeros((3,3), dtype='i')
     return (table)
 
+def game_loop(table, player):
+    return (table)
+
+def win_condition(table, player):
+    return  (player)
+
 if __name__ == "__main__":
     print("hello world")
     table = set_table()
-    print(table)
+    while (True):
+        table = game_loop(table, player)
+        player = win_condition(table, player)
+        if (player == 0):
+            break;
