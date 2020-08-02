7c6e2cc20fe908181d12be178337764119c4e364
s1mpleTEK <57884979+s1mpleTEK@users.noreply.github.com>
2020-08-02 23:54:41 +0200

    Added user name function

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,25 +1,31 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 
 import numpy as np
 
 player = 1  #default value
             #1 means O & 2 means X & 0 means someone win
 
+def set_usr():
+    usr =   [input("Player 1 enter your name: "),
+            input("Player 2 enter your name: "),
+            player]
+    return (usr)
+
 def set_table():
     table = np.zeros((3,3), dtype='i')
     return (table)
 
-def game_loop(table, player):
+def game_loop(table, usr):
     return (table)
 
-def win_condition(table, player):
-    return  (player)
+def win_condition(table, usr):
+    return (usr)
 
 if __name__ == "__main__":
-    print("hello world")
+    usr = set_usr()
     table = set_table()
     while (True):
-        table = game_loop(table, player)
-        player = win_condition(table, player)
-        if (player == 0):
-            break;
+        table = game_loop(table, usr)
+        usr = win_condition(table, usr)
+        if (usr[2] == 0):
+            break
