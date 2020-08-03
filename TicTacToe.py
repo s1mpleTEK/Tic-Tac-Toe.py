278105498803a5299424b5fdbb735148a6ec7305
s1mpleTEK <57884979+s1mpleTEK@users.noreply.github.com>
2020-08-03 06:02:47 +0200

    Added 8 possibilities on 8 to win and the draw possibilities; THE GAME DONE.END?

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,9 +3,6 @@
 import numpy as np
 import re
 
-turn = 1    #player turn
-            #1 means O & 2 means X & 0 means someone win & -1 means draw
-
 #win_patern: 8 solution, [[[line],[col]],[[line],[col]],[[line],[col]]]
 # wp =    [[[[0],[0]],[[0],[1]],[[0],[2]]],   #first line full
 #         [[[0],[0]],[[1],[0]],[[2],[0]]],    #first column full
@@ -16,10 +13,10 @@ turn = 1    #player turn
 #         [[[1],[0]],[[1],[1]],[[1],[2]]],    #second line full
 #         [[[2],[0]],[[2],[1]],[[2],[2]]],]   #third line full
 
-def set_user():                                     #set name for players
-    user =   [input("Player 1 enter your name: "),  #name of player 1
-            input("Player 2 enter your name: "),    #name of player 2
-            turn]                                   #player turn
+def set_user():                                         #set name for players
+    user =   [input("Player 1 enter your name: "),      #name of player 1
+            input("Player 2 enter your name: "),        #name of player 2
+            1]                                          #player turn: 1 means O & 2 means X & 0 means someone win & -1 means draw
 
     return (user)
 
@@ -64,29 +61,43 @@ def update_table(table, user, col, line):                       #update game boa
 
     return (table)
 
-def win_condition(table, user):
+def game_condition(table, user):                                                    #verify the win or draw and switch or not the player
     for i in range (3):
-        if user[2] % 2 == 1:
-            if table[i][0] == "O" and table[i][1] == "O" and table[i][2] == "O":
+        if user[2] % 2 == 1:                                                        #choose the good player
+            if table[i][0] == "O" and table[i][1] == "O" and table[i][2] == "O":    #verify all line for player 1
                 user[2] = 0
-            if table[0][i] == "O" and table[1][i] == "O" and table[2][i] == "O":
+            if table[0][i] == "O" and table[1][i] == "O" and table[2][i] == "O":    #verify all column for player 1
+                user[2] = 0
+            if table[0][0] == "O" and table[1][1] == "O" and table[2][2] == "O":    #verify digonal left top to right bottom for player 1
+                user[2] = 0
+            if table[0][2] == "O" and table[1][1] == "O" and table[2][0] == "O":    #verify digonal right top to left bottom for player 1
                 user[2] = 0
             if user[2] == 0:
                 print(user[0],"wins !")
                 return (user)
         else:
-            if table[i][0] == "X" and table[i][1] == "X" and table[i][2] == "X":
+            if table[i][0] == "X" and table[i][1] == "X" and table[i][2] == "X":    #verify all line for player 2
                 user[2] = 0
-            if table[0][i] == "X" and table[1][i] == "X" and table[2][i] == "X":
+            if table[0][i] == "X" and table[1][i] == "X" and table[2][i] == "X":    #verify all column for player 2
+                user[2] = 0
+            if table[0][0] == "X" and table[1][1] == "X" and table[2][2] == "X":    #verify digonal left top to right bottom for player 2
+                user[2] = 0
+            if table[0][2] == "X" and table[1][1] == "X" and table[2][0] == "X":    #verify digonal right top to left bottom for player 2
                 user[2] = 0
             if user[2] == 0:
                 print(user[1],"wins !")
                 return (user)
-    if user[2] % 2 == 1:
-        user[2] = 2
-    else:
-        user[2] = 1
-    return (user)
+
+    if "" in table[0] or "" in table[1] or "" in table[0]:                          #verify if the game board is not full
+        if user[2] % 2 == 1:
+            user[2] = 2
+        else:
+            user[2] = 1
+        return (user)
+    else:                                                                           #if the game board is full
+        print("Draw!")
+        user[2] = -1
+        return (user)
 
 if __name__ == "__main__":
     user = set_user()                                           #set name for player
@@ -95,6 +106,6 @@ if __name__ == "__main__":
     while (True):
         print(user[user[2]-1],"'s turn")                        #display player turn
         table = game_loop(table, user)                          #interaction with players
-        user = win_condition(table, user)                       #verify game board
+        user = game_condition(table, user)                      #verify the win or draw and switch or not the player
         if user[2] == 0 or user[2] == -1:                       #if turn equal 0 so the game stop because someone win or if turn equal -1 so the game stop because draw
             break
